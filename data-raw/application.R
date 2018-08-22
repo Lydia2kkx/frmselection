f1f40f8b3723973d7d38149e3601126c0bed9ae1
Yao Lu <yao.lu01@stud.uni-goettingen.de>
2018-08-22 18:30:23 +0200

    ...

--- data-raw/application.R
+++ data-raw/application.R
@@ -76,6 +76,32 @@ formula <- y ~ la(f1) + la(f2) #form the formula with two kinds of variables
 b <- bamlss(formula, data = d, family = frm_bamlss(link = "logit"), sampler = FALSE,
             optimizer = lasso, nlambda = 100, upper = 1e+08, lower = 1e+03, multiple = TRUE)
 coefi <- lasso.coef(b)
+lasso.select <- function(x, coefficent, threshold = 1e-3){
+  n <- length(coefficent) #n is the number of the coefficients
+  ncommon <- length(x[,-ind]) #the number of the common variables
+  nfactor <- length(levels(x[,ind]))  #find the number of levels of factor variables
+  #nfactor <- length(unique(x[,ind]))
+  #Extract the penalized parameters of common and factor variables separately.
+  tau.common <- coefficent[ncommon+1]
+  tau.factor <- coefficent[ncommon+nfactor+1]
+  #The relation between tau and lambda is an inverse relationship
+  lambda.common <- 1/tau.common
+  names(lambda.common) <- "mu.s.la(f1).lambda"
+  lambda.factor <- 1/tau.factor
+  names(lambda.factor) <- "mu.s.la(f2).lambda"
+  index.tau <- c(ncommon+1, ncommon+nfactor+1)
+  coefi.new <- coefficent[-index.tau] #the new coeffients doesn't contain the penalized parameters
+  lasso.index <- NULL
+  #Users can set this threshold as whatever they like. The default value is 1e-3
+  for(i in 1:length(coefi.new)){
+    if(abs(coefi.new[i]) < threshold){
+      coefi.new[i] = 0
+      lasso.index <- c(lasso.index, i)
+    }
+  }
+  return(list(lambda.common = lambda.common, lambda.factor = lambda.factor,
+              lasso.index = lasso.index, modified.coefficients = coefi.new))
+}
 lasso.select(x, coefi)
 
 
--- data-raw/lasso_selection.R
+++ data-raw/lasso_selection.R
@@ -19,6 +19,33 @@ coefi <- lasso.coef(b)
 #the factor variables(different levels of factor variables have different coefficients),
 #the lambda parameter(penalized with lasso) of factor variables and the intercept
 
+lasso.select <- function(x, coefficent, threshold = 1e-3){
+  n <- length(coefficent) #n is the number of the coefficients
+  ncommon <- length(x[,-ind]) #the number of the common variables
+  nfactor <- length(levels(x[,ind]))  #find the number of levels of factor variables
+  #nfactor <- length(unique(x[,ind]))
+  #Extract the penalized parameters of common and factor variables separately.
+  tau.common <- coefficent[ncommon+1]
+  tau.factor <- coefficent[ncommon+nfactor+1]
+  #The relation between tau and lambda is an inverse relationship
+  lambda.common <- 1/tau.common
+  names(lambda.common) <- "mu.s.la(f1).lambda"
+  lambda.factor <- 1/tau.factor
+  names(lambda.factor) <- "mu.s.la(f2).lambda"
+  index.tau <- c(ncommon+1, ncommon+nfactor+1)
+  coefi.new <- coefficent[-index.tau] #the new coeffients doesn't contain the penalized parameters
+  lasso.index <- NULL
+  #Users can set this threshold as whatever they like. The default value is 1e-3
+  for(i in 1:length(coefi.new)){
+    if(abs(coefi.new[i]) < threshold){
+      coefi.new[i] = 0
+      lasso.index <- c(lasso.index, i)
+    }
+  }
+  return(list(lambda.common = lambda.common, lambda.factor = lambda.factor,
+              lasso.index = lasso.index, modified.coefficients = coefi.new))
+}
+
 a <- lasso.select(x, coefi)
 a$lambda.common
 a$lambda.factor
